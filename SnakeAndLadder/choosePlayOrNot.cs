13dbab3fcd1cef513bd5b44e55cd7101a92e6bd5
Amit <amitkr24031995@gmail.com>
2021-09-08 12:49:25 +0530

    Added choosePlayOrNot

---
+++ SnakeAndLadder/choosePlayOrNot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace choosePlayOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int choose = 1;
+            Random random = new Random();
+            //Computation
+            int plays = random.Next(0, 2);
+            if (plays == choose)
+            {
+                Console.WriteLine("Play the Snake And Ladder");
+            }
+            else
+            {
+                Console.WriteLine("Don not Play the Snake And Ladder");
+            }
+        }
+    }
+}
