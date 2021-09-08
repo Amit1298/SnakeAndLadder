39759fc3cf64f119d1e5d73a23e8bbf2ad34b0ad
Amit <amitkr24031995@gmail.com>
2021-09-08 19:19:02 +0530

    Adding RollingDice

---
+++ SnakeAndLadder/RollingDice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RollingDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            Random rnd = new Random();
+
+            result = rnd.Next(1, 7);
+            Console.WriteLine(result);
+        }
+    }
+}
